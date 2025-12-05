77fd56937fd9d98a22f3671c4a01e99f2a97eb9e
Benjamin Jørgensen <benjaminjorgensen@MacBook-Air-tilhrende-Benjamin.local>
2025-12-05 15:48:00 +0100

    hej

--- Salling.R
+++ Salling.R
@@ -5,6 +5,24 @@ library(RMariaDB)
 library(purrr)
 library(DBI)
 
+###### NOTE "sådan opdater man en database i git." ####### 
+
+## 1 - IP adresse
+# start, husk at ændre IP adressen på MySQL, con_salling og inde i terminalen den nye når man launcher instance.
+
+## 2 - push fra R. terminal til linux 
+# git add Salling.sql eller tryk i toppen "hvor man gemmer" der er der et +- tegn 
+# git commit -m "Tilføjet Salling.sql" 
+# git push 
+
+## 3 - pull i terminalen på mac 
+# git pull
+# ls -la for at se om den rigtige sql er der
+# history  
+# mysql -u root -p --database=Salling_store < Salling.sql - samme måde bruges til at lave en database i linux
+
+## 4 - opdater MySQL 
+
 ###### KONFIG #######
 zip_code        <- "4000"
 target_store_id <- "769204f1-d8e4-41fb-8b9c-0b8b8f885376"
@@ -28,22 +46,17 @@ df  <- fromJSON(raw, flatten = TRUE)
 # liste med tilbud pr. butik
 offers <- df$clearances
 
-# brug store.id som navn på hvert liste-element
+# bruger store.id som navn på hvert liste-element
 names(offers) <- df$`store.id`
 
-# skriv store_id ind som kolonne i hver offers-tabel
+# skriver store_id ind som kolonne i hver offers-tabel
 for (i in seq_along(offers)) {
   offers[[i]]$store_id <- names(offers)[i]
 }
 
-# bind alle tilbud sammen til én dataframe
+# binder alle tilbud sammen til én dataframe
 df_offer <- dplyr::bind_rows(offers)
 rownames(df_offer) <- NULL
-
-# lille oversigt over butikker (valgfri)
-stores <- df %>%
-  select(`store.id`, store.name, store.address.street) %>%
-  distinct()
 
 # kun tilbud fra én butik (valgfri)
 netto_roskilde <- df_offer %>%
@@ -64,7 +77,7 @@ con_salling <- dbConnect(
 df_stores <- df %>%
   filter(`store.id` == target_store_id) %>%
   transmute(
-    store_id = `store.id`,          # ← nyt navn til DB
+    store_id = `store.id`,          
     brand    = store.brand,
     name     = store.name,
     type     = store.type,
@@ -85,7 +98,7 @@ dbWriteTable(
 
 ####### CLEARANCE_DATAFRAME – MED store_id #######
 
-clearance_df <- df_offer %>%
+clearance_df <- netto_roskilde %>%
   distinct() %>%
   mutate(
     across(
@@ -109,7 +122,7 @@ clearance_df <- df_offer %>%
     )
   ) %>%
   transmute(
-    store_id,                        # <-- matcher DB
+    store_id,                       
     ean              = product.ean,
     currency         = offer.currency,
     new_price        = offer.newPrice,
