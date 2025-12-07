e5095ae7cf103ee3711bbcf88dbc062105c58d4e
Benjamin Jørgensen <benjaminjorgensen@MacBook-Air-tilhrende-Benjamin.local>
2025-12-07 16:46:11 +0100

    Salling script + SQL med product_desc

--- Salling.R
+++ Salling.R
@@ -47,7 +47,7 @@ netto_roskilde <- df_offer %>%
 
 con_salling <- dbConnect(
   MariaDB(),
-  host     = "13.60.28.171",
+  host     = "localhost",
   dbname   = "Salling_store",
   user     = "dalremote",
   password = "Benja#1998"
